78d2f855c528876b1dc7fae0a380b812121dac5a
Ibragim Abbasov <blancduman@gmail.com>
2020-03-22 21:38:51 +0400

    for pullrequest

---
+++ build/precache-manifest.0532f9826ad5408a0db56b8ae4951565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88f3bf273fd21834d5242f9b2515a05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f17e48d6da8772f8e2b",
+    "url": "/static/css/main.7a92d149.chunk.css"
+  },
+  {
+    "revision": "0e7a4f6758b48ae40af4",
+    "url": "/static/js/2.4d5cc0cb.chunk.js"
+  },
+  {
+    "revision": "ebf314d28da7b1544af5821296fdb154",
+    "url": "/static/js/2.4d5cc0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f17e48d6da8772f8e2b",
+    "url": "/static/js/main.e847cac7.chunk.js"
+  },
+  {
+    "revision": "856c180f35d7078c29e3",
+    "url": "/static/js/runtime-main.be2697d1.js"
+  }
+]);
